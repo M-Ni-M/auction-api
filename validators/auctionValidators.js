2fd552337452c43bff187663a6dd45231afd7a31
ManAcquah <manacquah81@gmail.com>
2025-04-23 11:12:55 +0000

    category update

--- validators/auctionValidators.js
+++ validators/auctionValidators.js
@@ -5,7 +5,7 @@ export const auctionItemValidator = new Joi.object({
     title: Joi.string().required(),
     image: Joi.string().required(),
     description: Joi.string().required(),
-    category: Joi.string().required(),
+    category: Joi.string().required().valid('agricultural products', 'artisan crafts', 'electronics & gadgets', 'fashion & home decor'),
     startingBid: Joi.number().required(),
     endTime: Joi.date().required(),
 })
